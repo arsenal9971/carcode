fa3998240d9cbbd55fd78a2934c8bb8d42200494
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-04-21 12:47:15 +0000

    Added option parsing to carcode for level loading and replay testing

--- branches/gsoc08/carcode.py
+++ branches/gsoc08/carcode.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python
 from libcarcode import CarcodeApp, Car
 import sys
-    
-def main():
-    app = CarcodeApp(800, 600)
+from optparse import OptionParser
 
+parser = OptionParser()
+parser.add_option('-l', '--level', dest='level',
+    help='load an specific level file')
+parser.add_option('-m', '--monkey', action='store_true', 
+    dest='monkey', default=False,
+    help='record and replay, monkey see monkey do.')
+parser.add_option('-s', '--script', dest='script',
+    help='load an specific script file')
+
+(opts, args) = parser.parse_args()
+
+def setup_level(app):
     # Check if we have environment script
-    if len(sys.argv) > 1:
+    if opts.level:
         # Run level script
-        app.run_script(sys.argv[1])
+        app.run_script(opts.level)
     else:
         # Setup default environment
         car = Car(app.screen)
         app.arena.set_car(car)
+
+def main():
+    app = CarcodeApp(800, 600)
+    
+    setup_level()
     app.main_loop()
+    if opts.monkey:
+        setup_level()
+        app.rerun()
 
 if __name__ == '__main__':
     main()
