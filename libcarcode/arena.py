5f2503b7dd6b3c5e080d5474296d8dd60e2624e1
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-06-25 10:09:49 +0000

    Added new generic scripting mechanism and used it to load levels and car scripts

--- libcarcode/arena.py
+++ libcarcode/arena.py
@@ -10,26 +10,13 @@ import helpers
 WHITE = (250, 250, 250)
         
 class Arena:
-    def __init__(self, parent):
-        self.parent = parent
-        
+    def __init__(self):
         self.entities = []
-        self.car = None
         self.lines = []
         self.segments = []
     
     def set_car(self, car):
         self.car = car
-        self.parent.add_key(K_s, car.flip_engine)
-        self.parent.add_key(K_g, car.flip_gear)
-        self.parent.add_key(K_UP, car.accelerate)
-        self.parent.add_key(K_LEFT, car.steer_left)
-        self.parent.add_key(K_RIGHT, car.steer_right)
-        self.parent.add_key(K_DOWN, car.brake)
-        self.parent.add_key(K_h, car.honk)
-        self.parent.add_key(K_z, car.blinker_left_flip)
-        self.parent.add_key(K_c, car.blinker_right_flip)
-        self.parent.add_key(K_t, car.flip_tracer)
     
     def add_entity(self, entity):
         self.entities.append(entity)
