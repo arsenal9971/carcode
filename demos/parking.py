529343a001d4be98900ee6af96af6f5093cfd84d
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-06-26 06:39:39 +0000

    Updated parking demo to use colors instead of text for target parking zone

--- demos/parking.py
+++ demos/parking.py
@@ -12,13 +12,13 @@ def onCollision(entity):
         Console.write('Parked ok!')
         
 class ParkPlace:
-    def __init__(self, x, y, text = "", col = False):
+    def __init__(self, x, y, color=(0,0,0), col = False):
         self.x = x
         self.y = y
         self.l1 = Box(0, 0, 5, 70, WHITE)
         self.l2 = Box(0, 50, 5, 70, WHITE)
         self.l3 = Box(70, 0, 55, 5, WHITE)
-        self.text = Text(15, 30, text, WHITE)
+        self.l4 = Box(0, 5, 45, 70, color)
         self.bbox = BoundingBox(x+35, y+25, 50, 70)
         self.bbox.update()
         self.collisionable = col
@@ -28,17 +28,17 @@ class ParkPlace:
         self.l1.draw()
         self.l2.draw()
         self.l3.draw()
-        self.text.draw()
+        self.l4.draw()
         glPopMatrix()
         if DEBUG:
             self.bbox.draw()
     def update(self):
         pass
     
-Arena.add_entity(ParkPlace(100, 0, "1" ))
-Arena.add_entity(ParkPlace(100, 50, "2", col=True))
-Arena.add_entity(ParkPlace(100, 100, "3"))
-Arena.add_entity(ParkPlace(100, 150, "4"))
+Arena.add_entity(ParkPlace(100, 0))
+Arena.add_entity(ParkPlace(100, 50, color=(200,0,0), col=True))
+Arena.add_entity(ParkPlace(100, 100))
+Arena.add_entity(ParkPlace(100, 150))
 Arena.on_collision.subscribe(onCollision)
-Console.write("Park in parking place number 2")
+Console.write("Park over the red zone")
 
