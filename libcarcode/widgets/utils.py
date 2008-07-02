332d0da92f298560faf3b115dc9ef8638b1c5f81
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-02 22:15:52 +0000

    Added a correctly working and better performing Stencil based Clipper

--- libcarcode/widgets/utils.py
+++ libcarcode/widgets/utils.py
@@ -10,59 +10,46 @@ colors = [(1.0, 0.0, 0.0, 0.5), (0.0, 1.0, 0.0, 0.8), (0.0, 0.0, 1.0, 0.9)]
 
 class Clipper:
     
-    class __Clipper:
+    class __Clipper2:
         def __init__(self):
-            self.regions = []
-        
-        def __begin(self):
-            glClear(GL_STENCIL_BUFFER_BIT)
-            glEnable(GL_STENCIL_TEST)
-            
-            i = 0
-            glColorMask(GL_FALSE,GL_FALSE,GL_FALSE,GL_FALSE)
-            
-            if len(self.regions) == 1:
-                reg = self.regions[0]
-                glStencilOp(GL_REPLACE,GL_REPLACE,GL_REPLACE)
-                glStencilFunc(GL_ALWAYS,1,1)
-                glRecti(*reg)
-            else:
-                reg = self.regions[0]
-                glStencilOp(GL_REPLACE,GL_REPLACE,GL_REPLACE)
-                glStencilFunc(GL_ALWAYS,1,1)
-                glRecti(*reg)
-                
-                reg = self.regions[1]
-                glStencilOp(GL_ZERO,GL_ZERO,GL_ZERO)
-                glStencilFunc(GL_EQUAL,1,1)
-                glRecti(*reg)
-                
-                reg = self.regions[0]
-                glStencilOp(GL_REPLACE,GL_KEEP,GL_ZERO)
-                glStencilFunc(GL_EQUAL,1,1)
-                glRecti(*reg)
-                
-            glColorMask(GL_TRUE,GL_TRUE,GL_TRUE,GL_TRUE)
-            glStencilFunc(GL_EQUAL,1,1)
-            glStencilOp(GL_KEEP,GL_KEEP,GL_KEEP)
+            self.regions = 0
                 
         def begin(self, region):
-            self.regions.insert(0,region)
-            self.__begin()
+            if self.regions == 0:
+                glClear(GL_STENCIL_BUFFER_BIT)
+                glEnable(GL_STENCIL_TEST)
+                op = (GL_REPLACE,GL_REPLACE,GL_REPLACE)
+                func = (GL_ALWAYS,1,1)
+            else:
+                op = (GL_KEEP, GL_KEEP,GL_INCR)
+                func = (GL_EQUAL,1,1)
+            
+            glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE)
+            
+            glStencilOp(*op)
+            glStencilFunc(*func)
+            glRecti(*region)
+            
+            self.regions += 1
+            
+            glColorMask(GL_TRUE,GL_TRUE,GL_TRUE,GL_TRUE)
+            glStencilFunc(GL_EQUAL,self.regions, self.regions)
+            glStencilOp(GL_KEEP,GL_KEEP,GL_KEEP)
         
         def end(self):
-            self.regions.pop(0)
-            if len(self.regions) == 0:
+            self.regions -= 1
+            if self.regions == 0:
                 glDisable(GL_STENCIL_TEST)
             else:
-                glDisable(GL_STENCIL_TEST)
-                self.__begin()
+                glColorMask(GL_TRUE,GL_TRUE,GL_TRUE,GL_TRUE)
+                glStencilFunc(GL_EQUAL,self.regions, self.regions)
+                glStencilOp(GL_KEEP,GL_KEEP,GL_KEEP)
 
     __singleton = None
     
     def __init__(self):
         if Clipper.__singleton is None:
-            Clipper.__singleton = Clipper.__Clipper()
+            Clipper.__singleton = Clipper.__Clipper2()
         self.__singleton = Clipper.__singleton
         #self.__dict__['_Clipper__singleton'] = Clipper.__singleton
     def begin(self, *args):
