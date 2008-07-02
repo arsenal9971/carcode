d970a944e56464b0626f2bf7818418309669d931
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-02 22:11:49 +0000

    Label calculates its own size according the text and font

--- libcarcode/widgets/label.py
+++ libcarcode/widgets/label.py
@@ -8,16 +8,26 @@ class Label:
         self.text = text
         self.pos = list(pos)
         self.color = color
+        self.size = [len(text) * 8, 13]
+    
+    def set_text(self, text):
+        self.text = text
+        self.size = [len(text) * 8, 13]
+        
     def events(self, event):
         return False
+    
     def draw(self):
         glPushMatrix()
         glTranslatef(self.pos[0], self.pos[1], 0)
+        
         if len(self.color) == 3:
             glColor3f(*self.color)
         else:
             glColor4f(*self.color)
+            
         glRasterPos3i(0, 0, 0)
         for c in self.text:
             glutBitmapCharacter(GLUT_BITMAP_8_BY_13, ord(c))
+            
         glPopMatrix()
