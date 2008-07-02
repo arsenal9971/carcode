43001477f16386c9fb970d99c311cfecb58b1342
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-02 22:13:23 +0000

    Changed Button label to a generic widget to be able to use images or other geometry

--- libcarcode/widgets/button.py
+++ libcarcode/widgets/button.py
@@ -8,29 +8,39 @@ from events import EventDispatcher
 from label import Label
 
 class Button:
-    def __init__(self, text, pos, size, color):
-        ly = (size[1] / 2) + 4
-        lx = (size[0] - (len(text) * 8)) / 2
-        self.label = Label(text, (lx, ly), COLOR_WHITE)
+    def __init__(self, contents, pos, size, color):
+        lx = (size[0] / 2) - (contents.size[0] / 2)
+        ly = (size[1] / 2) - (contents.size[1] / 2)
+        
+        self.contents = contents
+        self.contents.pos = [lx, ly]
+        
         self.color = color
         self.pos = pos
         self.size = size
         self.onClick = EventDispatcher()
+        
     def events(self, event):
         if event.type == MOUSEBUTTONUP:
             inX = lambda x: x >= self.pos[0] and x <= self.pos[0]+self.size[0]
             inY = lambda y: y >= self.pos[1] and y <= self.pos[1]+self.size[1]
+            
             if inX(event.pos[0]) and inY(event.pos[1]):
                 self.onClick.dispatch(self)
                 return True
+            
         return False
+        
     def draw(self):
         glPushMatrix()
         glTranslatef(self.pos[0], self.pos[1], 0)
+        
         if len(self.color) == 3:
             glColor3f(*self.color)
         else:
             glColor4f(*self.color)
         glRecti(0, 0, self.size[0], self.size[1])
-        self.label.draw()
+        
+        self.contents.draw()
+        
         glPopMatrix()
