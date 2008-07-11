6e952687b5d742d82226715167f589d402abeabb
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-11 19:02:28 +0000

    Updated packaging versions

--- carcode.py
+++ carcode.py
@@ -3,12 +3,14 @@ from libcarcode import CarcodeApp, Car
 import sys
 from optparse import OptionParser
 
+VERSION = "3.0a1"
+
 parser = OptionParser()
 parser.add_option('-l', '--level', dest='level',
     help='load an specific level file')
-parser.add_option('-m', '--monkey', action='store_true', 
-    dest='monkey', default=False,
-    help='record and replay, monkey see monkey do.')
+parser.add_option('-v', '--version', action='store_true', 
+    dest='version', default=False,
+    help='display carcode version and exit.')
 parser.add_option('-s', '--script', dest='script',
     help='load an specific script file')
 
@@ -23,6 +25,10 @@ def setup_level(app):
         app.load_script(opts.script)
 
 def main():
+    if opts.version:
+        print "carcode",  VERSION
+        sys.exit(0)
+        
     app = CarcodeApp(800, 600)
     
     setup_level(app)
