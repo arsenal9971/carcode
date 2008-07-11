ba291c2da30c5845a7428a684562d696aaf813ad
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-11 19:01:43 +0000

    Updated parking demo to use latest scripting facilities

--- demos/parking.py
+++ demos/parking.py
@@ -6,39 +6,51 @@ WHITE = (255,255,255)
 car = Arena.get_car()
 DEBUG = False
 
-def onCollision(entity):
-    if entity.bbox.contains(car.bbox):
+def onCollision(entity,  event):
+    if event[1]:
+        if abs(car.angle) > 2:
+            Console.clear()
+            Console.write('Now park the car horizontally!')
+        else:
+            Console.clear()
+            Console.write('Parked ok!')
+    else:
         Console.clear()
-        Console.write('Parked ok!')
+        Console.write("Park over the red zone")
         
-class ParkPlace:
+class ParkPlace(ccEntity):
     def __init__(self, x, y, color=(0,0,0), col = False):
+        ccEntity.__init__(self,  0)
         self.x = x
         self.y = y
-        self.l1 = Box(0, 0, 5, 70, WHITE)
-        self.l2 = Box(0, 50, 5, 70, WHITE)
-        self.l3 = Box(70, 0, 55, 5, WHITE)
-        self.l4 = Box(0, 5, 45, 70, color)
-        self.bbox = BoundingBox(x+35, y+25, 50, 70)
-        self.bbox.update()
+        self.l1 = Box((x, y), (70, 5), WHITE)
+        self.l2 = Box((x, y+50), (70,  5), WHITE)
+        self.l3 = Box((x+ 70, y), (5,  55), WHITE)
+        self.l4 = Box((x, y+5), (70,  45), color,  True)
+        
         self.collisionable = col
+        
+        self.get_region = self.l4.get_region
+        self.get_center = self.l4.get_center
+        self.evt_collision = self.l4.evt_collision
+        
     def draw(self):
-        glPushMatrix()
-        glTranslatef(self.x, self.y, 0.0)
         self.l1.draw()
         self.l2.draw()
         self.l3.draw()
         self.l4.draw()
-        glPopMatrix()
-        if DEBUG:
-            self.bbox.draw()
+        
     def update(self):
         pass
     
+park = ParkPlace(100, 50, color=(200,0,0), col=True)
+
 Arena.add_entity(ParkPlace(100, 0))
-Arena.add_entity(ParkPlace(100, 50, color=(200,0,0), col=True))
+Arena.add_entity(park)
 Arena.add_entity(ParkPlace(100, 100))
 Arena.add_entity(ParkPlace(100, 150))
-Arena.on_collision.subscribe(onCollision)
+
+park.evt_collision.subscribe(onCollision)
+
 Console.write("Park over the red zone")
 
