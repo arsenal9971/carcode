76e65e6d79645f03903d05cd3aedbe261147405a
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-15 07:47:21 +0000

    Integrated widget packing code in a single class supporting new Widget protocol

--- libcarcode/widgets/layout.py
+++ libcarcode/widgets/layout.py
@@ -1,71 +1,52 @@
 
-class VerticalPack:
-    """Vertical widget packing
-        Aligns and resizes a list of widgets in a vertical fashion.
-    """
-    def __init__(self, pos, size, padding = 1, margin = 1):
-        self.pos = pos
-        self.size = size
-        self.entities = []
-        self.padding = padding
-        self.margin = margin
-        self.visible = True
-        
-    def refresh(self):
-        ecount = len(self.entities)
-        eW = self.size[0] - (self.margin * 2)
-        eH = (self.size[1] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
-        eX = self.margin + self.pos[0]
-        eY = self.margin + self.pos[1]
-        for entity in self.entities:
-            entity.pos = [eX, eY]
-            entity.resize((eW, eH))
-            eY += eH + self.padding
-            
-    def resize(self, size):
-        self.size = size
-        self.refresh()
-        
-    def add_entity(self, obj):
-        self.entities.append(obj)
-        self.refresh()
-    
-    def events(self, event):
-        for entity in self.entities:
-            if entity.events(event):
-                return True
-        return False
-        
-    def draw(self):
-        for entity in self.entities:
-            entity.draw()
-            
+from constants import *
+from widget import Widget
 
-class HorizontalPack:
-    """Horizontal widget packing
-        Aligns and resizes a list of widgets in a horizontal fashion.
+class Pack(Widget):
+    """ Widget packing
+    Aligns and resizes a list of widgets in a vertical or horizontal fashion.
     """
-    def __init__(self, pos, size, padding = 1, margin = 1):
-        self.pos = pos
-        self.size = size
+    def __init__(self, orientation = VERTICAL,  padding = 1, margin = 1,  *args,  **kargs):
+        """ Pack
+        
+        @param orientation packing orientation, VERTICAL by default
+        @param padding interwidget padding, 1 by default
+        @param margin pack margin, 1 by default
+        @param pos tuple with widget position (x, y)
+        @param size tuple with widget size (width, height)
+        """
+        Widget.__init__(self,  *args,  **kargs)
+        
         self.entities = []
+        self.orientation = orientation
         self.padding = padding
         self.margin = margin
         self.visible = True
-        
+            
+    # TODO: replace orientation checks with lambdas
     def refresh(self):
         ecount = len(self.entities)
-        eW = (self.size[0] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
-        eH = self.size[1] - (self.margin * 2) 
+        
+        if self.orientation == VERTICAL:
+            eW = self.size[0] - (self.margin * 2)
+            eH = (self.size[1] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
+        else:
+            eW = (self.size[0] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
+            eH = self.size[1] - (self.margin * 2) 
+            
         eX = self.margin + self.pos[0]
         eY = self.margin + self.pos[1]
         for entity in self.entities:
-            entity.pos = [eX, eY]
-            entity.resize((eW, eH))
-            eX += eW + self.padding
-    
-    def resize(self, size):
-        self.size = size
+            entity.set_pos( [eX, eY])
+            entity.set_size((eW, eH))
+            
+            if self.orientation == VERTICAL:
+                eY += eH + self.padding
+            else:
+                eX += eW + self.padding
+            
+    def set_size(self,  size):
+        Widget.set_size(self,  size)
         self.refresh()
         
     def add_entity(self, obj):
