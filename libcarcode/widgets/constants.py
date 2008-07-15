c14569062eb5942d7b91bbc622cc3343eceeebbe
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-15 07:48:19 +0000

    Updated constants

--- libcarcode/widgets/constants.py
+++ libcarcode/widgets/constants.py
@@ -2,3 +2,11 @@
 # Color constants
 COLOR_WHITE = (1.0,1.0,1.0,1.0)
 COLOR_BLACK = (0.0,0.0,0.0,1.0)
+
+# Scrollbar orientations
+VERTICAL = 1
+HORIZONTAL = 2
+
+# Default widgets colors
+WINDOW_BACKCOLOR = (0.2,  0.2,  0.2,  1.0)
+BUTTON_BACKCOLOR = (0.4,  0.4,  0.4,  1.0)
