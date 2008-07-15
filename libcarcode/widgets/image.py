6d82bea0e7db0f01616d427c6b557ec8812b172b
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-15 08:12:43 +0000

    Updated Image to subclass Widget

--- libcarcode/widgets/image.py
+++ libcarcode/widgets/image.py
@@ -1,20 +1,31 @@
 from OpenGL.GL import *
-from OpenGL.GLUT import glutBitmapCharacter, GLUT_BITMAP_8_BY_13
 
 import pygame
 from pygame.locals import *
 
 from events import EventDispatcher
+from widget import Widget
 
-class Image:
-    def __init__(self, pos, filename):
-        self.pos = pos
+class Image(Widget):
+    """ Image widget """
+    def __init__(self, filename,  *args,  **kargs):
+        """ Image
+        @param filename filename of the image
+        """
+        Widget.__init__(self,  *args,  **kargs)
+        
+        # Raw image data from pygame
         img = pygame.image.load(filename)
         img = img.convert_alpha()
+        
         w, h = img.get_size()
         self.size = (w, h)
-        self.l = glGenLists(1)
-        glNewList(self.l, GL_COMPILE)
+        
+        # Generate new compiled list
+        self.imgList = glGenLists(1)
+        
+        # Begin adding points to the compiled list
+        glNewList(self.imgList, GL_COMPILE)
         glBegin(GL_POINTS)
         for x in xrange(w):
             for y in xrange(h):
@@ -23,17 +34,25 @@ class Image:
                 glVertex2i(x, y)
         glEnd()
         glEndList()
+        
         del img
         self.visible = True
+        
     def __del__(self):
         try:
-            glDeleteLists(self.l, 1)
+            glDeleteLists(self.imgList, 1)
         except:
             pass
+        
+    def set_size(self,  size):
+        # Image widgets cannot be resized
+        pass
+        
     def events(self, event):
         return False
+        
     def draw(self):
         glPushMatrix()
         glTranslatef(self.pos[0], self.pos[1], 0)
-        glCallList(self.l)
+        glCallList(self.imgList)
         glPopMatrix()
