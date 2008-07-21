2e096de910f93bed9cf21af38151bbeac3572070
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-21 22:47:27 +0000

    Simplified Condition interface with Goal class

--- libcarcode/events.py
+++ libcarcode/events.py
@@ -1,3 +1,5 @@
+from widgets.events import EventDispatcher
+
 class AND:
     """ Conditional AND for events """
     def __init__(self,  con1,  st1,  con2,  st2):
@@ -60,6 +62,61 @@ class OR:
 
 scores = [1,  2,  3,  4]
 
+class Goal:
+    """ Class describing a certain goal """
+    def __init__(self,  desc,  testfunc,  state = 0):
+        """Goal
+        
+        @param desc string describing the objective
+        @param testfunc function that test of a given condition returning a boolean
+        @param state state number to set if condition is true
+        """
+        self.desc = desc
+        self.testfunc = testfunc
+        self.state = 0
+        self.onTest = EventDispatcher()
+        
+    def test(self):
+        """ Test goal function 
+        
+        @returns tuple of boolean and state
+        """
+        res = self.testfunc()
+        
+        self.onTest.dispatch(self,  res,  self.state)
+        return res, self.state
+        
+class Chain:
+    """ Chain of goals """
+    def __init__(self,  *args):
+        """ Chain """
+        
+        if len(args) > 0:
+            self.goals = list(args)
+        else:
+            self.goals = []
+        self.name = ""
+    
+    def add_objective(self,  obj):
+        """Add a goal to the chain 
+        
+        @param obj goal object
+        """
+        self.goals.append(obj)
+    
+    def test(self):
+        """ Test goals 
+       @returns tuple with test bolean and state
+       """
+        state = 0
+        for obj in self.goals:
+            val,  st = obj.test()
+            if not val:
+                return False,  state
+            else:
+                state = st
+        return True,  state
+        
 class Score:
     def __init__(self,  name,  scorefunc,  values):
         """ Score
