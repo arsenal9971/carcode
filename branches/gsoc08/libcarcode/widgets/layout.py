999848ce06d6cff45d90ef272242ccdade177189
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-21 07:37:46 +0000

    Added expand parameter to Pack, widgets which don't want to be expanded can set this to false

--- branches/gsoc08/libcarcode/widgets/layout.py
+++ branches/gsoc08/libcarcode/widgets/layout.py
@@ -28,37 +28,72 @@ class Pack(Widget):
         ecount = len(self.entities)
         
         if self.orientation == VERTICAL:
-            eW = self.size[0] - (self.margin * 2)
-            eH = (self.size[1] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
+            eSize = self.size[1] - (self.margin * 2)
         else:
-            eW = (self.size[0] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
+            eSize = self.size[0] - (self.margin * 2)
+        
+        ecount = 0
+        for entity,  expand in self.entities:
+            if expand:
+                ecount += 1
+            else:
+                w,  h = entity.get_size()
+                if self.orientation == VERTICAL:
+                    eSize -= h
+                else:
+                    eSize -= w
+        
+        
+        if self.orientation == VERTICAL:
+            eW = self.size[0] - (self.margin * 2)
+            #eH = (self.size[1] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
+            if ecount:
+                eH = (eSize - ((len(self.entities)-1) * self.padding)) / ecount
+            else:
+                eH = 0
+        else:
+            #eW = (self.size[0] - (self.margin * 2) - ((ecount -1) * self.padding)) / ecount
+            if ecount:
+                eW = (eSize - ((len(self.entities)-1) * self.padding)) / ecount
+            else:
+                eW = 0
             eH = self.size[1] - (self.margin * 2) 
             
         eX = self.margin + self.pos[0]
         eY = self.margin + self.pos[1]
-        for entity in self.entities:
+        for entity,  expand in self.entities:
             entity.set_pos( [eX, eY])
-            entity.set_size((eW, eH))
             
-            if self.orientation == VERTICAL:
-                eY += eH + self.padding
+            if expand:
+                entity.set_size((eW, eH))
+                
+                if self.orientation == VERTICAL:
+                    eY += eH + self.padding
+                else:
+                    eX += eW + self.padding
             else:
-                eX += eW + self.padding
+                w,  h = entity.get_size()
+                if self.orientation == VERTICAL:
+                    entity.set_size((eW, h))
+                    eY += h + self.padding
+                else:
+                    entity.set_size((w, eH))
+                    eX += w + self.padding
             
     def set_size(self,  size):
         Widget.set_size(self,  size)
         self.refresh()
         
-    def add_entity(self, obj):
-        self.entities.append(obj)
+    def add_entity(self, obj,  expand = True):
+        self.entities.append((obj,  expand))
         self.refresh()
     
     def events(self, event):
-        for entity in self.entities:
+        for entity,  expand in self.entities:
             if entity.events(event):
                 return True
         return False
         
     def draw(self):
-        for entity in self.entities:
+        for entity,  expand in self.entities:
             entity.draw()
