c8d128c4e8e461a90254e4b5d461c9906c89282d
clsdaniel <clsdaniel@03d1c737-1b38-0410-8f88-e99976d769a8>
2008-07-27 14:47:56 +0000

    Changed public goal and score APIs from CarcodeApp Proxy, we only have set_goals and set_scores now, added docstrings

--- branches/gsoc08/libcarcode/level_proxy.py
+++ branches/gsoc08/libcarcode/level_proxy.py
@@ -9,19 +9,47 @@ class ArenaProxy:
         return self.__arena__.car
 
 class AppProxy:
+    """ Carcode application interface for scripts """
     def __init__(self, app):
         self.__app__ = app
+        
     def add_key(self, key, action):
+        """ Add a key binding
+        
+        @param key pygame key code
+        @param action function to call when key is pressed
+        """
         self.__app__.add_key(key, action)
+        
     def quit(self):
+        """ Send the quit signal to the application """
         self.__app__.quit()
-    def set_conditions(self,  condition):
-        self.__app__.condition = condition
-        self.__app__.goalWindow.set_goals(condition)
+        
+    def set_goals(self,  goals):
+        """ Set the level goals 
+        
+        @param goals list of Goal objects
+        """
+        self.__app__.goals = goals
+        self.__app__.goalWindow.set_goals(goals)
+        
+    def set_scores(self,  scores):
+        """ Set level score criteria 
+        
+        @param scores list of Score objects
+        """
+        self.__app__.scoreboard = scores
         
     def get_state(self):
+        """ Get current state as defined by goals
+        
+        @returns state return code from last accomplished goal
+        """
         return self.__app__.state
-    def add_score(self,  score):
-        self.__app__.scoreboard.append(score)
+        
     def get_game_time(self):
+        """ Get the time the simulation has been on 
+        
+        @returns time in seconds that the simulation has been on.
+        """
         return self.__app__.game_time
